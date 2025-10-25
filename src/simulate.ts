03d6ed4cff2ff7a344d9c5f977ef635ecb65993e
luanbrandao <luanbrandao4@gmail.com>
2025-10-24 21:49:16 -0300

    Delete simpleAnalyzer

--- src/simulate.ts
+++
@@ -1,9 +0,0 @@
-import { TradeSimulator } from './simulator/trade-simulator';
-import SimpleAnalyzer from './analyzers/simpleAnalyzer';
-
-async function runSimulation() {
-  const simulator = new TradeSimulator(SimpleAnalyzer, 1000, 'SOLUSDT');
-  await simulator.simulate();
-}
-
-runSimulation();
