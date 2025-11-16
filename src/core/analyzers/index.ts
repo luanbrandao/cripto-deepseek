5cda6e7f384bfe24dad9e322e3f09c7c82024063
luanbrandao <luanbrandao4@gmail.com>
2025-11-16 07:03:18 -0300

    refactor: remove unused files

--- src/core/analyzers/index.ts
+++ src/core/analyzers/index.ts
@@ -4,18 +4,13 @@
  */
 
 // === TECHNICAL ANALYZERS ===
-export { default as Analyzer123 } from './technical/pattern-123';
 export { default as EmaAnalyzer } from './technical/ema-analyzer';
-export { MomentumAnalyzer } from './technical/momentum-analyzer';
-export { VolumeAnalyzer } from './technical/volume-analyzer';
-export { default as SupportResistanceAnalyzer } from './technical/support-resistance-analyzer';
 
 // === FACTORIES ===
-export { TechnicalAnalyzerFactory } from './factories/technical-analyzer-factory';
 // Note: DeepSeekAnalysisFactory temporarily disabled due to import issues
 
 // === UNIFIED SERVICES ===
 // Note: UnifiedDeepSeekAnalyzer and UltraConservativeAnalyzer temporarily disabled due to import issues
 
 // === TYPES ===
-export type { AnalyzerType, TechnicalAnalysisResult, MarketDataInput } from './factories/technical-analyzer-factory';
+// Note: Types disabled due to removed analyzers and factories
